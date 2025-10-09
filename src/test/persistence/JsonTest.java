8e7baf6b40b2fada3b4c4031701a418ee7e9e77d
Paul Carter <pcarter@cs.ubc.ca>
2025-10-09 11:29:20 -0700

    Modified test to improve coverage

--- src/test/persistence/JsonTest.java
+++ src/test/persistence/JsonTest.java
@@ -9,5 +9,6 @@ public class JsonTest {
     protected void checkThingy(String name, Category category, Thingy thingy) {
         assertEquals(name, thingy.getName());
         assertEquals(category, thingy.getCategory());
+        assertEquals(category + ": " + name, thingy.toString());
     }
 }
